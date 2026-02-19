5867c5219a5100ab65db653caf4f78b244f871d5
avinashpandey2703 <pandeyavinash123456789@gmail.com>
2026-02-19 10:38:06 +0530

    Initial commit - LangChain models project

---
+++ ChatModels/chatmodel_anthropic.py
@@ -0,0 +1,9 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+model=ChatAnthropic(model='claude-sonnet-4-5-20250929')
+
+result=model.invoke('what is the capital of india')
+
+print(result.content)
